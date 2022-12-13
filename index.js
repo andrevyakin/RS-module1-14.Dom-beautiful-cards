06a7e81465424ab90c4ba2200c00c8d7a5dd60ed
Андрей <andrevyakin@gmail.com>
2022-12-13 19:13:10 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const URL_LIST_USERS = "https://jsonplaceholder.typicode.com/users";
+const loader = document.querySelector(".progress");
+
+const createElement = (tag, attributes, parent) => {
+    const el = document.createElement(tag);
+    if (attributes.class)
+        attributes.class.forEach(i => el.classList.add(i));
+    if (attributes.text)
+        el.textContent = attributes.text;
+    Array.from(document.querySelectorAll(parent)).at(-1).append(el);
+}
+
+const listTemplate = list => {
+    createElement("ul", {class: ["collapsible"]}, "#cards");
+    list.forEach(card => {
+        createElement("li", {}, ".collapsible");
+        createElement("div", {class: ["collapsible-header", "teal", "accent-1"]}, "li");
+        createElement("i", {
+            class: ["material-icons", "teal-text", "text-darken-2"],
+            text: "assignment_ind"
+        }, ".collapsible-header");
+        createElement("span", {text: `${card.name}`}, ".collapsible-header ");
+        createElement("span", {
+            class: ["badge", "white-text", "teal", "lighten-2"],
+            text: `${card.id}`
+        }, ".collapsible-header");
+        createElement("div", {class: ["collapsible-body", "teal", "lighten-4"]}, "li");
+        createElement("table", {}, ".collapsible-body");
+        createElement("thead", {}, "table");
+        createElement("tr", {}, "thead");
+        createElement("th", {text: "Username"}, "tr");
+        createElement("th", {text: "Email"}, "tr");
+        createElement("th", {text: "Phone"}, "tr");
+        createElement("th", {text: "Website"}, "tr");
+        createElement("tbody", {}, "table");
+        createElement("tr", {}, "tbody");
+        createElement("td", {text: `${card.username}`}, "tr");
+        createElement("td", {text: `${card.email}`}, "tr");
+        createElement("td", {text: `${card.phone}`}, "tr");
+        createElement("td", {text: `${card.website}`}, "tr");
+    });
+}
+
+const toggleLoader = () => {
+    loader.classList.toggle("hide");
+}
+fetch(URL_LIST_USERS)
+    .then((response) => {
+        toggleLoader();
+        return response.json();
+    })
+    .then(listTemplate)
+    //Magic materialize.
+    .then(() => {
+        const elems = document.querySelectorAll(".collapsible");
+        const instances = M.Collapsible.init(elems);
+    })
+    .catch((error) => {
+        toggleLoader();
+        console.error("Что-то пошло не так...", error);
+    })
+    .finally(() => {
+        toggleLoader();
+    });
